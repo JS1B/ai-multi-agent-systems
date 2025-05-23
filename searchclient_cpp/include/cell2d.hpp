099c99204e9b8492ba7500aee73e20dd6ae2d798
Maksym Ratajczyk <m.ratajczyk6@student.uw.edu.pl>
2025-05-23 22:07:50 +0200

    Small performance improvements

--- searchclient_cpp/include/cell2d.hpp
+++ searchclient_cpp/include/cell2d.hpp
@@ -5,17 +5,18 @@
 // TODO: use int_fast8_t instead of int if performance is an issue
 
 struct Cell2D {
-    int r, c; // row and column
+    //int r, c; // row and column
+    int_fast8_t r, c; // row and column
 
     Cell2D() = default;
     Cell2D(int row, int col) : r(row), c(col) {}
 
-    bool operator==(const Cell2D& o) const { return r == o.r && c == o.c; }
-    bool operator!=(const Cell2D& o) const { return r != o.r || c != o.c; }
+    inline bool operator==(const Cell2D& o) const { return r == o.r && c == o.c; }
+    inline bool operator!=(const Cell2D& o) const { return r != o.r || c != o.c; }
 
-    Cell2D& operator+=(const Cell2D& o) { r += o.r; c += o.c; return *this; }
-    Cell2D& operator-=(const Cell2D& o) { r -= o.r; c -= o.c; return *this; }
+    inline Cell2D& operator+=(const Cell2D& o) { r += o.r; c += o.c; return *this; }
+    inline Cell2D& operator-=(const Cell2D& o) { r -= o.r; c -= o.c; return *this; }
 
-    Cell2D operator+(const Cell2D& o) const { return Cell2D(r + o.r, c + o.c); }
-    Cell2D operator-(const Cell2D& o) const { return Cell2D(r - o.r, c - o.c); }
+    inline Cell2D operator+(const Cell2D& o) const { return Cell2D(r + o.r, c + o.c); }
+    inline Cell2D operator-(const Cell2D& o) const { return Cell2D(r - o.r, c - o.c); }
 };
